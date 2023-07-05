e53325a85b0b39bffe50d5075209925619aa8eae
sevel.liu <sevel.liu@cloudminds.com>
2023-07-05 16:29:23 +0800

    1.add(support flexible)

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ docker rm python_server_demo
 docker rmi python_server_demo:1.0
 docker build -t python_server_demo:1.0 .
 docker volume create huggingface_cache
-docker run -d -p 50251:50251 -p 50252:50252 -v huggingface_cache:/opt/huggingface_cache --name python_server_demo python_server_demo:1.0
+docker run -d -p 50251:50251 -p 50252:50252 -v huggingface_cache:/opt/huggingface_cache --name python_server_demo --restart=always  python_server_demo:1.0
